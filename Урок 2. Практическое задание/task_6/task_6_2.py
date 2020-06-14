c632a6888950612148c8bd50560bf195ce2df3b5
Evgueniii <63573470+Evgueniii@users.noreply.github.com>
2020-06-14 17:15:26 +0300

    Update task_6_2.py

--- Урок 2. Практическое задание/task_6/task_6_2.py
+++ Урок 2. Практическое задание/task_6/task_6_2.py
@@ -7,3 +7,24 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ РЕКУРСИЮ
 """
+from random import random
+
+def quest_number_func(quest_number, user_tries=1):
+    if user_tries > 10:
+        return print(f'Игра окончена, правильный ответ {quest_number}')
+    try:
+        user_answer = int(input(f'{user_tries} попытка: '))
+        if user_answer < quest_number:
+            print('Загаданное число больше')
+        elif user_answer > quest_number:
+            print('Загаданное число меньше')
+        elif user_answer == quest_number:
+            print(f'Вы отгадали, правильный ответ {quest_number}')
+            user_tries = 10
+        user_tries += 1
+        return quest_number_func(quest_number, user_tries)
+    except ValueError:
+        print('Введите число от 1 до 100')
+        quest_number_func(quest_number, user_tries)
+
+quest_number_func(round(random() * 100))
