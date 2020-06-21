4b4956979c13cb5e58a309c47c78956b7e64f5e6
Evgueniii <63573470+Evgueniii@users.noreply.github.com>
2020-06-21 11:56:53 +0300

    Update task_6.py

--- Урок 3.Практическое задание/task_6.py
+++ Урок 3.Практическое задание/task_6.py
@@ -13,3 +13,27 @@
 Массив: [88, 58, 50, 77, 49, 6, 42, 67, 14, 79]
 Сумма элементов между минимальным (6)  и максимальным (88) элементами: 234
 """
+
+from random import randint
+
+r = [randint(0, 88) for n in range(10)]
+print(f'Массив: {r}')
+
+min = 0
+max = 0
+step = 1
+sum = 0
+
+for i in r:
+    if r[min] > i:
+        min = r.index(i)
+    elif r[max] < i:
+        max = r.index(i)
+
+if max - min < 0:
+    step = -1
+
+for i in r[min + step:max:step]:
+    sum += i
+
+print(f'Сумма элементов между минимальным {r[min]}', f' и максимальным {r[max]} элементами: {sum}')
