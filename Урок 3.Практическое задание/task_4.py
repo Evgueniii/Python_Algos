8038c8ea4636401ec21aced6e2a0f4470b9c3e7b
Evgueniii <63573470+Evgueniii@users.noreply.github.com>
2020-06-21 11:55:59 +0300

    Update task_4.py

--- Урок 3.Практическое задание/task_4.py
+++ Урок 3.Практическое задание/task_4.py
@@ -3,3 +3,15 @@
 
 Подсказка: можно применить ф-цию max с параметром key
 """
+
+from random import randint
+
+r = [randint(0, 50) for _ in range(20)]
+print(f'Массив: {r}')
+
+max_index = 0
+for i in r:
+    if r.count(max_index) < r.count(i):
+        max_index = r.index(i)
+
+print(f'Число {r[max_index]}, встречается {r.count(max_index)} раз')
